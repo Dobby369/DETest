b911be0ad8c425977b5728e14ad9c6c49a641ed6
Tomogochi <malkova.ol939@gmail.com>
2024-05-21 23:52:57 +0300

    session 1

---
+++ DE2/Pages/Login.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DE2.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Login.xaml
+    /// </summary>
+    public partial class Login : Page
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            User user = Singeton.DB.User.FirstOfDefault(u =>
+                u.username = username.Text && u.password = password.Password);
+            MainWindow mainWindow = Window.GetWindow(this) as MainWindow;
+            mainWindow.frame.Navigate(new Page1());
+        }
+
+        private void RegBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = Window.GetWindow(this) as MainWindow;
+            mainWindow.frame.Navigate(new Registration());
+        }
+    }
+}
